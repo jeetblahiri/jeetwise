feb22ba9ec4044da410394fc11313506742e843a
jeetblahiri <d23146@students.iitmandi.ac.in>
2026-04-21 01:18:51 +0530

    Inline Pages app into single file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(({ mode, command }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -7,18 +8,10 @@ export default defineConfig(({ mode, command }) => {
 
   return {
     base: basePath,
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
     build: {
       rollupOptions: {
-        output: {
-          entryFileNames: 'assets/[name].js',
-          chunkFileNames: 'assets/[name].js',
-          assetFileNames: 'assets/[name][extname]',
-          manualChunks: {
-            react: ['react', 'react-dom'],
-            firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
-          },
-        },
+        output: {},
       },
     },
   };
